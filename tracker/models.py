16b23fb60beb8922a5cc8f8a0bd56e97a5521843
Dennis Kinanga <denniskinanga6@gmail.com>
2025-01-22 13:49:10 +0300

    display tractions

--- tracker/models.py
+++ tracker/models.py
@@ -26,4 +26,8 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f'{self.type} of {self.amount} on {self.date} by {self.user}'
+    
+
+    class Meta:
+        ordering = ['-date']
 
